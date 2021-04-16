f59af8d2dc0db9f6f370de40049dca124eb23391
lary-coder <laryssarib@gmail.com>
2021-04-16 15:12:43 -0300

    "you"

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+"/*
+ * File: 6-abs.c
+ * Auth: Laryssa Ribeiro
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
