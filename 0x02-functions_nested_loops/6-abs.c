acd53f022db458d3dc99f81cb55063c4b67d6dfc
lary-coder <laryssarib@gmail.com>
2021-04-16 16:37:18 -0300

    "you2"

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,20 @@
-"/*
+/*
  * File: 6-abs.c
  * Auth: Laryssa Ribeiro
 */
 
 #include "holberton.h"
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
-	return (0);
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
