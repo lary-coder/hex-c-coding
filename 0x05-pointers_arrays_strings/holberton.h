1069eb43a0f6a2a9ff71b81422d6be08c34dc199
lary-coder <laryssarib@gmail.com>
2021-05-04 18:47:15 -0300

    holberton

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,19 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/**
- * File: holberton.h
- * Auth: Laryssa Ribeiro
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -23,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+#endif /* HOLBERTON_H */
