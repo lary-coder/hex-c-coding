5426c19eb5ff97103e987ecdf092806a74f2d030
lary-coder <laryssarib@gmail.com>
2021-05-05 15:43:10 -0300

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,31 @@
-/*
- * File: 6-cap_string.c
- * Auth: Laryssa Ribeiro
- */
-
 #include "holberton.h"
-
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
-char *cap_string(char *str)
+* cap_string - a function that capitalizes all words of a string
+* @s: string
+* Return: s
+*/
+char *cap_string(char *s)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+	while (s[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] -= 32;
+		}
+		else
+			s[i] = s[i];
+		i++;
 	}
-
-	return (str);
+	return (s);
+}
