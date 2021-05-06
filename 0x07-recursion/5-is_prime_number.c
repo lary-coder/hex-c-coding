ea60b97d835b9bde119fb7285828d0fb4d8d3463
lary-coder <laryssarib@gmail.com>
2021-05-06 16:37:10 -0300

    is_prime_number

--- 0x07-recursion/5-is_prime_number.c
+++ 0x07-recursion/5-is_prime_number.c
@@ -2,47 +2,30 @@
  * File: 5-sqrt_recursion.c
  * Auth: Laryssa Ribeiro
  */
-
 #include "holberton.h"
-
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
-
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
- *
- * Return: If the number has a natural square root - the square root.
- *         If the number does not have a natural square root - -1.
- */
-int find_sqrt(int num, int root)
+* is_prime_number - check if n is prime number
+* @n: a integer
+* Return: 1 if is prime and o otherwise
+*/
+int is_prime_number(int n)
 {
-	if ((root * root) == num)
-		return (root);
-
-	if (root == num / 2)
-		return (-1);
-
-	return (find_sqrt(num, root + 1));
-}
-
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to return the square root of.
- *
- * Return: If n has a natural square root - the natural square root of n.
- *         If n does not have a natural square root - -1.
- */
-int _sqrt_recursion(int n)
-{
-	int root = 0;
-
-	if (n < 0)
-		return (-1);
-
-	if (n == 1)
+	if (n == 2 || n == 3 || n == 5 || n == 7)
+	{
 		return (1);
-
-	return (find_sqrt(n, root));
+	}
+	else if (n == -1 || n == 1)
+	{
+		return (0);
+	}
+	else if ((n % 2 == 0) || (n % 3 == 0) || (n % 4 == 0)
+|| (n % 5 == 0) || (n % 6 == 0) || (n % 7 == 0))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
+
